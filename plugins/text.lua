cb688d62cd684db1b2f1005dc1b4de84079efab7
EMCTeamIR <EMCTeamIR@users.noreply.github.com>
2016-03-17 11:35:41 +0330

    Create text.lua

---
+++ plugins/text.lua
@@ -0,0 +1,30 @@
+do
+
+function run(msg, matches)
+
+local fuse = 'ID : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nusername :@ ' .. msg.from.username  ..'\n\ntext:\n\n\n' .. matches[1]
+local fuses = '!printf user#id' .. msg.from.id
+
+
+    local text = matches[1]
+                 local sends0 = send_msg('chat#70690378', fuse, ok_cb, false)
+
+ return 'your text send!'
+
+
+
+end
+
+end
+return {
+  description = "feedback",
+
+  usage = "feedback : send maseage to admins with bot",
+  patterns = {
+    "^[Ff]eedback (.*)$"
+
+  },
+  run = run
+}
+
+end
